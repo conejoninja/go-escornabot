5149a337f6ba4c9108254d19d25204061ab75f30
Daniel Esteban <conejo@theninjabunny.com>
2019-12-21 20:50:22 +0100

    added .Configure()

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ import (
 )
 
 func main() {
-	//motors := easystepper.NewDual(machine.D5, machine.D7, machine.D6, machine.D8, machine.D9, machine.D11, machine.D10, machine.D12, 200, 60)
 	motors := easystepper.NewDual(machine.D0, machine.D7, machine.D1, machine.D9, machine.D10, machine.D12, machine.D11, machine.D13, 200, 60)
+	motors.Configure()
 	buttons := input.NewAnalogButtons(machine.A1)
 	bzrPin := machine.A0
 	bzrPin.Configure(machine.PinConfig{Mode: machine.PinOutput})
